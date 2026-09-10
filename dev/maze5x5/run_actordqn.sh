a25e0a3b2da907591ac41fe4989ced14ebc9d48d
Jay <480284856@qq.com>
2026-09-10 20:02:22 +0200

    baseline of random choice II

--- dev/maze5x5/run_actordqn.sh
+++ dev/maze5x5/run_actordqn.sh
@@ -1,18 +1,18 @@
-# python main_actor.py --tensorboard-log-dir "../logs/actorqdn/maze5x5/large/v1" \
-#     --width 5 \
-#     --height 5 \
-#     --max-episode-steps-eval 25 \
-#     --total-time-steps 819200 \
-#     --learning-starts 409600 \
-#     --seed 42
-python main_actor.py --tensorboard-log-dir "../logs/actorqdn/maze5x5/large/v2" \
+python -m IncompleteBaseline.dev.main_actor --tensorboard-log-dir "IncompleteBaseline/dev/logs/actorqdn/maze5x5/v1" \
+    --width 5 \
+    --height 5 \
+    --max-episode-steps-eval 25 \
+    --total-time-steps 819200 \
+    --learning-starts 409600 \
+    --seed 42
+python -m IncompleteBaseline.dev.main_actor --tensorboard-log-dir "IncompleteBaseline/dev/logs/actorqdn/maze5x5/v2" \
     --width 5 \
     --height 5 \
     --max-episode-steps-eval 25 \
     --total-time-steps 819200 \
     --learning-starts 409600 \
     --seed 43
-python main_actor.py --tensorboard-log-dir "../logs/actorqdn/maze5x5/large/v3" \
+python -m IncompleteBaseline.dev.main_actor --tensorboard-log-dir "IncompleteBaseline/dev/logs/actorqdn/maze5x5/v3" \
     --width 5 \
     --height 5 \
     --max-episode-steps-eval 25 \
