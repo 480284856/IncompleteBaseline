d981dbf756fc9f8e09529bd680c10260c788c903
Jay <480284856@qq.com>
2026-09-21 13:16:13 +0200

    maze 5x5 step 250

--- dev/maze5x5/run_actordqn.sh
+++ dev/maze5x5/run_actordqn.sh
@@ -1,10 +1,10 @@
 cd /Users/jay/Desktop/RL/project/
 
-python -m IncompleteBaseline.dev.main_actor --tensorboard-log-dir "IncompleteBaseline/dev/logs/actorqdn/maze5x5/normal/small_lr/v1" \
+python -m IncompleteBaseline.dev.main_actor --tensorboard-log-dir "IncompleteBaseline/dev/logs/actorqdn/maze5x5/normal/step-250/v1" \
     --width 5 \
     --height 5 \
-    --max-episode-steps-eval 128 \
-    --max-episode-steps 2048 \
+    --max-episode-steps-eval 250 \
+    --max-episode-steps 250 \
     --total-time-steps 1228800 \
     --learning-starts 819200 \
     --seed 42
